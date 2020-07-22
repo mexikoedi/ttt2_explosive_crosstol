c4bf89ab166c5386dc3dc7700ce47fe668d76574
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2020-07-22 20:08:46 +0200

    Initial Upload

---
+++ ttt2_explosive_crosstol/lua/lang/deutsch/crosstol.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference( "deutsch" )
+L[ "ttt2_explosive_crosstol_name" ] = "Explosive Crosstol"
+L[ "ttt2_explosive_crosstol_desc" ] = "Pistole die explosive Armbrust Munition verschießt."
+L[ "ttt2_explosive_crosstol_help1" ] = "{primaryfire} Schießen und Treffen."
+L[ "ttt2_explosive_crosstol_help2" ] = "{secondaryfire} Entspannende 30 Sekunden Musik."
---
+++ ttt2_explosive_crosstol/lua/lang/english/crosstol.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference( "english" )
+L[ "ttt2_explosive_crosstol_name" ] = "Explosive Crosstol"
+L[ "ttt2_explosive_crosstol_desc" ] = "Pistol that fires explosive crossbow ammunition."
+L[ "ttt2_explosive_crosstol_help1" ] = "{primaryfire} Shoot and hit."
+L[ "ttt2_explosive_crosstol_help2" ] = "{secondaryfire} Relaxing 30 Seconds music."
