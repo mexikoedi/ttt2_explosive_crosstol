e9d29e4793da81274f403321af2ca4c7d6cf305e
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-17 22:08:17 +0100

    formatted code + updated folder name

---
+++ ttt2_explosive_crosstol/lua/terrortown/lang/ru/crosstol.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference("ru")
+L["ttt2_explosive_crosstol_name"] = "Взрывоопасный кросстол"
+L["ttt2_explosive_crosstol_desc"] = "Пистолет, стреляющий разрывными боеприпасами для арбалета."
+L["ttt2_explosive_crosstol_help1"] = "{primaryfire} Выстрелить и попасть."
+L["ttt2_explosive_crosstol_help2"] = "{secondaryfire} Расслабляющая музыка на 30 секунд."
