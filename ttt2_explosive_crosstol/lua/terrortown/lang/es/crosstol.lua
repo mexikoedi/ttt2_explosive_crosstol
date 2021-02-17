889f4489eccf356587233c9a0ed9e9fbcb76d2f6
mexikoedi <60551569+mexikoedi@users.noreply.github.com>
2021-02-17 22:07:56 +0100

    formatted code + updated folder name

---
+++ ttt2_explosive_crosstol/lua/terrortown/lang/es/crosstol.lua
@@ -0,0 +1,5 @@
+L = LANG.GetLanguageTableReference("es")
+L["ttt2_explosive_crosstol_name"] = "Pistollesta Explosiva"
+L["ttt2_explosive_crosstol_desc"] = "Pistola que dispara flechas explosivas."
+L["ttt2_explosive_crosstol_help1"] = "{primaryfire} Disparar."
+L["ttt2_explosive_crosstol_help2"] = "{secondaryfire} Para 30 segundos de música relajante."
